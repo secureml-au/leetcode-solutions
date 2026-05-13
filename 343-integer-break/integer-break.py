561b47e6f21981480bfb51e845fee78c97eaaca1
Au <auamores3@gmail.com>
2026-05-13 08:33:05 +0800

    Time: 0 ms (100.00%) | Memory: 19.2 MB (54.39%) - LeetSync

---
+++ 343-integer-break/integer-break.py
@@ -0,0 +1,9 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n <= 3: return n - 1
+        dp = [0] * (n + 1)
+        dp[2] = 2
+        dp[3] = 3
+        for i in range(4, n + 1):
+            dp[i] = max(2 * dp[i-2], 3 * dp[i-3])
+        return dp[n]
