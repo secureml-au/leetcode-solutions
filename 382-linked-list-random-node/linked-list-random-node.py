a2adc9984a6d2ddec6866cbe9023889bf212848f
Au <auamores3@gmail.com>
2026-05-14 09:56:38 +0800

    Time: 4 ms (94.66%) | Memory: 23.2 MB (72.77%) - LeetSync

---
+++ 382-linked-list-random-node/linked-list-random-node.py
@@ -0,0 +1,11 @@
+import random
+
+class Solution:
+    def __init__(self, head: Optional[ListNode]):
+        self.arr = []
+        while head:
+            self.arr.append(head.val)
+            head = head.next
+
+    def getRandom(self) -> int:
+        return random.choice(self.arr)
